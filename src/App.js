dc8d252df027a6288aa57429730eea1a047f8e9c
alexpryp <alexpryp@gmail.com>
2021-01-03 01:45:46 +0200

    Added nav section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import react from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            
+        </div>
+    );
+}
+
+export default HeroSection;
