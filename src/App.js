fa4557cceaf502c137c7c20c790b2dbe8278e7f3
alexpryp <alexpryp@gmail.com>
2021-01-04 16:30:25 +0200

    Added Footer section and some pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div id="main-content">
+        <Home />
+      </div>
+      <Footer />
     </div>
   );
 }
